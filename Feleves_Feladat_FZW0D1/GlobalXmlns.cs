eb421355b4f99a029ae4537267f8cbd204016928
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-11-12 18:30:59 +0100

    directory rename

--- Feleves_Feladat_FZW0D1/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Feleves_Feladat_FZW0D1")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Feleves_Feladat_FZW0D1.Pages")]
