0f4db54f11ab792c00841294bd1669bb05151dcb
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-11-12 20:48:17 +0100

    DbSet creation

---
+++ MusicLister.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using MusicLister.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicLister.Infrastructure
+{
+    public class DatabaseContext(DbContextOptions options):DbContext(options)
+    {
+        public DbSet<Artist> Artists { get; set; }
+
+        public DbSet<Song> Songs { get; set; } 
+    
+    
+    }
+}
