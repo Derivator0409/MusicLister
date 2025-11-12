3a3389ca18fc88c91c8765e0888af8aadac7befe
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-11-12 18:28:27 +0100

    Database entity base

---
+++ MusicLister.Model/Artist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLister.Models
+{
+    public class Artist
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [StringLength(150)]
+        [Required]
+        public string Name { get; set; }
+    
+         virtual ICollection<Song> Songs { get; set; }
+    
+    
+    }
+}
---
+++ MusicLister.Model/Song.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLister.Models
+{
+    public class Song
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [StringLength(150)]
+        [Required]
+        public string Title { get; set; }
+        [StringLength(150)]
+        [Required]
+        public TimeSpan Length { get; set; }
+        [Required]
+        public int ArtistID { get; set; }
+
+    
+    
+    
+    }
+}
