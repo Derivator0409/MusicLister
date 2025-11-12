727e7e74358099108d9ca7a7841c6837ad41cb83
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-11-12 20:03:30 +0100

    IArtistService interface

---
+++ MusicLister.Services/IArtistService.cs
@@ -0,0 +1,22 @@
+﻿using MusicLister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLister.Services
+{
+    public  interface IArtistService
+    {
+        public Task CreateArtistAsync(Artist newArtist);
+
+        public Task DeleteArtistAsync(int ID);
+
+        public Task UpdateArtistAsync(int ID, Artist UpdateArtist);
+
+        public Task<List<Artist>> GetAllAsync();
+
+        public Task<Artist> GetAsync(int ID);
+    }
+}
