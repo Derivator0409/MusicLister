01ad5c4343319ba3e7985a97732a890d35df2f26
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-11-12 19:57:16 +0100

    ISongService create

---
+++ MusicLister.Services/ISongService.cs
@@ -0,0 +1,23 @@
+﻿using MusicLister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLister.Services
+{
+    public interface ISongService
+    {
+        public Task CreateSongAsync(Song newSong);
+
+        public Task DeleteSongAsync(int ID);
+
+        public Task UpdateSongAsync(int ID, Song UpdateSong);
+
+        public Task <List<Song>> GetAllAsync();
+
+        public Task<Song> GetAsync(int ID);
+    
+    }
+}
