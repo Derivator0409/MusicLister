5a731204651ae4f155805030099bada24c526353
FZW0D1@stud.uni-obuda.hu <FZW0D1@stud.uni-obuda.hu>
2025-11-13 20:07:10 +0100

    MVVM started

---
+++ MusicLister.Main/ViewModels/EditArtistViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feleves_Feladat_FZW0D1.ViewModels
+{
+    public partial class EditArtistViewModel:ObservableObject
+    {
+    
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ MusicLister.Main/Views/EditArtistPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Feleves_Feladat_FZW0D1.Views;
+
+public partial class CreateSongPage : ContentPage
+{
+	public CreateSongPage()
+	{
+		InitializeComponent();
+	}
+}
